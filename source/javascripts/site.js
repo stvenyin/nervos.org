99df273d8b013b29140ee2bc927721a536056117
classicalliu <classicalliu@gmail.com>
2018-07-20 17:23:08 +0800

    faq page

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,25 @@
 // This is where it all goes :)
+
+document.addEventListener("DOMContentLoaded", function() {
+  var btns = document.getElementsByClassName('n-dropdown-toggle');
+  var className = "n-open";
+
+  for(var i = 0; i < btns.length; i++) {
+
+
+    (function(index) {
+      var currentBtn = btns[index];
+      var currentList = currentBtn.parentElement.getElementsByClassName('n-dropdown-list')[0];
+      var classList = currentList.classList;
+      currentBtn.addEventListener("click", function() {
+        if(classList.contains(className)) {
+          // remove
+          classList.remove(className);
+        } else {
+          // add
+          classList.add(className);
+        }
+      })
+    })(i);
+  }
+});
